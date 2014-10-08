69dddd0cb46db0d4d8f8bd9897e82c5552af967a
daervo <daervo@y7mail.com>
2014-10-08 14:44:12 +1100

    didnt commit anything last time

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -152,6 +152,8 @@ public class Controller extends HttpServlet {
 			request.setAttribute("links", googleDrive.getFileDownloadLinks());
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
 		}
 		return "listFiles.jsp";
 	}
